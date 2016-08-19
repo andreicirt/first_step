0a01537c983ce4fdd1e0ab88ed317bc65bc96679
andrei.cirt <andrei.cirt@emag.ro>
2016-08-19 15:37:35 +0300

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrei.cirt
+ * Date: 8/19/2016
+ * Time: 13:14
+ */
+
+//Database Constants
+
+define('DB_SERVER',"localhost")? null : define("DB_SERVER","localhost");
+define('DB_USER',"root")? null :define("DB_USER","root");
+define('DB_PASS',"")? null :define("DB_PASS","");
+define('DB_NAME',"problemaphp")? null :define("DB_NAME","problemaphp");
